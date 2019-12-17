e5bc6129dfb3a810b70aea5d116f10dca3d9830d
Rasmus Munk <munk1@live.dk>
2019-12-17 14:12:47 +0100

    Add mail config to init

--- projects/__init__.py
+++ projects/__init__.py
@@ -9,7 +9,7 @@ from flask_mail import Mail
 from flask_nav import Nav
 from projects_base.base import base_blueprint
 from projects.models import User
-from projects.forms import ProjectFormManager, DefaultProjectForm
+from projects.forms import ProjectFormManager
 from projects.conf import config
 
 app = Flask(__name__)
@@ -28,9 +28,24 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+project_manager = ProjectFormManager()
+
+# Onetime authentication reset token salt
+app.config['ONETIME_TOKEN_SALT'] = os.urandom(24)
+
+# Email application server
+app.config['MAIL_SERVER'] = config.get('MAIL', 'server')
+app.config['MAIL_PORT'] = config.get('MAIL', 'port')
+app.config['MAIL_USE_TLS'] = bool(config.get('MAIL', 'use_tls'))
+app.config['MAIL_USE_SSL'] = bool(config.get('MAIL', 'use_ssl'))
+app.config['MAIL_USERNAME'] = config.get('MAIL', 'username')
+app.config['MAIL_PASSWORD'] = config.get('MAIL', 'password')
+
+# Application ADMINS_EMAIL
+app.config['ADMINS_EMAIL'] = config.get('MAIL', 'admins').split(',')
+
 # Connect mail
 mail = Mail(app)
-project_manager = ProjectFormManager()
 
 # Setup navbar
 nav = Nav()
@@ -39,8 +54,6 @@ import projects.nav
 import projects.views
 app.register_blueprint(projects_blueprint)
 
-# Onetime authentication reset token salt
-app.config['ONETIME_TOKEN_SALT'] = os.urandom(24)
 
 # If debug option
 if app.debug:
